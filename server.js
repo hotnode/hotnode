cbd113ce089b2d4eb3f236c38b785e844a9fe3ba
KyleK <kylekatarnls@gmail.com>
2015-06-14 18:14:14 +0200

    Auto-rescue, instant config refresh

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var configFile = './config.json';
 
 var config = require(configFile);
 
-var loadTime = new Date().getTime();
-
 process.on('uncaughtException', function (e) {
-  console.warn(e.stack || e);
+  if(! e.code || e.code !== 'ECONNREFUSED') {
+    console.warn(e.stack || e);
+  }
 });
 
 var fs = require('fs');
@@ -25,13 +25,15 @@ proxy.on('proxyReq', function(proxyReq, req, res, options) {
 
 var http, target;
 
+function getSlavesList() {
+  var slaves = config.slaves || config.slave;
+  return typeof(slaves) !== 'object' ? [slaves] : slaves;
+}
+
 function setTarget() {
   if(config.target.indexOf('slave') === 0) {
     var index = config.target.replace(/[^0-9]/g, '');
-    var slaves = config.slaves || config.slave;
-    if(typeof(slaves) !== 'object') {
-      slaves = [slaves];
-    }
+    var slaves = getSlavesList();
     if(typeof(slaves[index]) === 'string') {
       if(target !== slaves[index]) {
         target = target = slaves[index];;
@@ -66,6 +68,53 @@ if(config.protocole === 'https') {
   http = require('http');
 }
 
+function rescue(i) {
+  i = i || 0;
+  var slaves = getSlavesList();
+  if(slaves.length <= i) {
+    console.warn('master and all slaves seem to be dead!');
+  } else {
+    var slave = slaves[i];
+    alive(slave, function (slaveIsAlive) {
+      if(slaveIsAlive) {
+        target = slave;
+        console.warn('master is dead, rescue mode on ' + target);
+      } else {
+        rescue(i + 1);
+      }
+    });
+  }
+}
+
+function alive(host, done) {
+  var expire = setTimeout(function () {
+    expire = null;
+    done(false);
+  }, config.rescue.timeout * 1000);
+  http.get(host + config.rescue.alive, function (res) {
+    if(res.statusCode === 200) {
+      var contents = '';
+      res.on('data', function (chunk) {
+        contents += chunk;
+      });
+      res.on('end', function () {
+        if(expire) {
+          done(contents === 'alive');
+          clearTimeout(expire);
+        }
+      });
+    } else {
+      if(expire) {
+        done(false);
+        clearTimeout(expire);
+      }
+    }
+  }).on('error', function (err) {
+    done(false);
+    clearTimeout(expire);
+  });
+}
+
 function getNewConfig() {
   fs.readFile(configFile, function (err, contents) {
     if(! err) {
@@ -74,6 +123,11 @@ function getNewConfig() {
         if(typeof(newConfig) === 'object') {
           config = newConfig;
           setTarget();
+          if(config.rescue.enabled && target === config.master) {
+            alive(config.master, function (masterIsAlive) {
+              masterIsAlive || rescue();
+            });
+          }
         }
       } catch(e) {
         console.warn(e);
@@ -89,24 +143,23 @@ function redirect(res, location, permanent) {
   return res.end();
 }
 
+fs.watch('.', getNewConfig);
+getNewConfig();
+
 var server = http.createServer(function(req, res) {
   var host = (req.headers['host'] || '').toLowerCase();
   if(config.redirection.enabled) {
     return redirect(res, target + (config.redirection.appendUrl ? req.url : ''), config.redirection.type === 'permanent');
   }
-  if(config['enforce-www'] && host.indexOf('www.') !== 0) {
+  if(config.enforceWww && host.indexOf('www.') !== 0) {
     return redirect(res, config.protocole + '://www.' + host + req.url, true);
   }
   proxy.web(req, res, {
     target: target
   }, function (e) {
+    console.log('Error on target ' + target);
     console.warn(e);
   });
-  var now = new Date().getTime();
-  if(now - loadTime > config.ttl * 1000) {
-    loadTime = now;
-    getNewConfig();
-  }
 });
 
 (config.listen || (process.env.PORT || '80').split(/[,;|\s]+/g)).forEach(server.listen.bind(server));
